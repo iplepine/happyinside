36d50585846a09b6e6a481173d9ff4938e563824
basil <basil@aui-MacBookPro.local>
2025-07-13 13:27:54 +0900

    명언 완전 랜덤 노출

--- app/lib/shared/services/quote_service.dart
+++ app/lib/shared/services/quote_service.dart
@@ -1,10 +1,11 @@
+import 'dart:math';
+
 import '../../core/constants/quotes.dart';
 
 class QuoteService {
   Map<String, String> getQuoteOfTheDay() {
-    // 이달의 몇 번째 날인지를 기준으로 인덱스를 계산 (1~31)
-    final dayOfMonth = DateTime.now().day;
-    final index = dayOfMonth % quotes.length;
+    final random = Random();
+    final index = random.nextInt(quotes.length);
     return quotes[index];
   }
 }
